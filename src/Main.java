7cfab43df0a7e147dea446076e383bb9b8227216
mohammaddawodzai <mohammaddawodzai2022@gmail.com>
2023-04-05 08:47:03 -0400

    The basic solar test01 system is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystemT1 systemT1=new SolarSystemT1();
+
+        systemT1.sun="1";
+        systemT1.moon="4";
+        systemT1.planets="9";
+        
+
+    }
+}
---
+++ src/SolarSystemT1.java
@@ -0,0 +1,9 @@
+public class SolarSystemT1 {
+
+    String sun;
+    String moon;
+    String planets;
+
+
+
+}
